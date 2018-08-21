fd49cb0ad800bdb829bc2c7dfc3ff0eb5698f300
帅小天 <sss@t203.com>
2018-08-21 16:32:50 +0800

    关于wf的相关

---
+++ WorkflowDemo/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace WorkflowDemo
+{
+
+    class Program
+    {
+        enum 工作流类型 { 顺序工作流, 流程图工作流, 状态机工作流 }
+
+        static Activity 获取工作流对象(工作流类型 type)
+        {
+            switch (type)
+            {
+                case 工作流类型.顺序工作流:
+                    return new SequentialNumberGuessWorkflow();
+                case 工作流类型.流程图工作流:
+                    return new FlowchartNumberGuessWorkflow();
+                case 工作流类型.状态机工作流:
+                    return new StateMachineNumberGuessWorkflow();
+                default:
+                    // 默认为顺序工作流
+                    return new FlowchartNumberGuessWorkflow();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            AutoResetEvent syncEvent = new AutoResetEvent(false);
+            AutoResetEvent idleEvent = new AutoResetEvent(false);
+
+            var inputs = new Dictionary<string, object>() { { "MaxNumber", 100 } };
+
+            Activity activity = 获取工作流对象(工作流类型.状态机工作流);
+
+            WorkflowApplication wfApp = new WorkflowApplication(activity, inputs);
+
+            wfApp.Completed = delegate (WorkflowApplicationCompletedEventArgs e)
+            {
+                int Turns = Convert.ToInt32(e.Outputs["Turns"]);
+                Console.WriteLine("恭喜，你猜对了 {0} 转的数字。", Turns);
+
+                syncEvent.Set();
+            };
+
+            wfApp.Aborted = delegate (WorkflowApplicationAbortedEventArgs e)
+            {
+                Console.WriteLine(e.Reason);
+                syncEvent.Set();
+            };
+
+            wfApp.OnUnhandledException = delegate (WorkflowApplicationUnhandledExceptionEventArgs e)
+            {
+                Console.WriteLine(e.UnhandledException.ToString());
+                return UnhandledExceptionAction.Terminate;
+            };
+
+            wfApp.Idle = delegate (WorkflowApplicationIdleEventArgs e)
+            {
+                idleEvent.Set();
+            };
+
+            wfApp.Run();
+
+            // 循环直到工作流程完成。
+            WaitHandle[] handles = new WaitHandle[] { syncEvent, idleEvent };
+            while (WaitHandle.WaitAny(handles) != 0)
+            {
+                // 收集用户输入并恢复书签。
+                bool validEntry = false;
+                Console.WriteLine("请输入一个整数。");
+                while (!validEntry)
+                {
+                    int Guess;
+                    if (!Int32.TryParse(Console.ReadLine(), out Guess))
+                    {
+                        Console.WriteLine("请输入一个整数。");
+                    }
+                    else
+                    {
+                        validEntry = true;
+                        // 把输入值传递给工作流，第一个参数为属性名，第二个为值
+                        wfApp.ResumeBookmark("EnterGuess", Guess);
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ WorkflowDemo/ReadInt.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Activities;
+
+namespace WorkflowDemo
+{
+
+    public sealed class ReadInt : NativeActivity<int>
+    {
+        // 定义一个字符串类型的活动输入参数
+        [RequiredArgument]
+        public InArgument<string> BookmarkName { get; set; }
+
+        // 如果活动返回值，则从 NativeActivityContext<TResult>
+        // 并从 Execute 方法返回该值
+        protected override void Execute(NativeActivityContext context)
+        {
+            string name = BookmarkName.Get(context);
+
+            if (name == string.Empty)
+            {
+                throw new ArgumentException("BookmarkName不能是空字符串。",
+                    "BookmarkName");
+            }
+
+            context.CreateBookmark(name, new BookmarkCallback(OnReadComplete));
+        }
+
+
+        //NativeActivity派生的活动通过调用执行脱机操作
+        //System.Activities.NativeActivityContext上定义的CreateBookmark重载之一
+        //必须覆盖CanInduceIdle属性并返回true。
+        protected override bool CanInduceIdle
+        {
+            get { return true; }
+        }
+
+        void OnReadComplete(NativeActivityContext context, Bookmark bookmark, object state)
+        {
+            this.Result.Set(context, Convert.ToInt32(state));
+        }
+    }
+}
